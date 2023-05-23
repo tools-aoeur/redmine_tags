f6e3959fbc37d8ad6344851a69f5c2ef8bee2669
Hare Ram Rai <guru.java.ram@gmail.com>
2023-05-23 13:14:12 +0200

    fix: Change tags collation from utf8_bin to default database collation (#4)
    Co-authored-by: Hare Ram Rai <hareram.rai@alpsalpine.com>

---
+++ db/migrate/20230522114900_reset_collation_for_tags.rb
@@ -0,0 +1,28 @@
+class ResetCollationForTags < ActiveRecord::Migration[6.0]
+  def up
+    if ActsAsTaggableOn::Utils.using_mysql?
+      # Get the default character set
+      default_charset = ActiveRecord::Base.connection.execute("SELECT @@character_set_database").first[0]
+
+      # Get the default collation
+      default_collation = ActiveRecord::Base.connection.execute("SELECT @@collation_database").first[0]
+
+      execute(
+        "ALTER TABLE tags MODIFY name varchar(255) CHARACTER SET #{default_charset} COLLATE #{default_collation};"
+      )
+
+      execute(
+        "ALTER TABLE tags MODIFY name varchar(255) DEFAULT NULL;"
+      )
+      
+    end
+  end
+
+  def down
+    if ActsAsTaggableOn::Utils.using_mysql?
+      execute(
+        'ALTER TABLE tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;'
+      )
+    end
+  end
+end
